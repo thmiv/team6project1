f601563977cbfdd43723a3ef1bf1277fdbb348a9
Thomas <thmiv4@gmail.com>
2018-08-09 12:21:03 -0500

    made array push to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,28 @@
 // javascript (Spoontacular Food API)
 
+var database = firebase.database();   // variable for access firebase
 var userPantry = [];    // array to hold food supplies in user's pantry
 var userIngredients = [];    // array to hold ingredients inputed by user
 
-function getPantry(addPantryItem) {
-    userPantry.push(addPantryItem);
+function getPantry() {
+    var newFBitem;  // get newly added item from Firebas
+    database.ref("/Pantry").on("child_added", function(snapshot) {
+        newFBitem = snapshot.val().addPantryItem;
+        userPantry.push(newFBitem);
+        $("#inv-list").append(newFBitem + "<br>");
+    });
+    
     console.log(userPantry);
 }
 
 $( document ).ready(function() {
 
+    getPantry();
+
     $("#submit").on("click", function(event){
         event.preventDefault();
-        var newItem = $("#pantry-input").val().trim();
-        getPantry(newItem);
+        var addPantryItem = $("#pantry-input").val().trim();
+        database.ref("/Pantry").push({addPantryItem});
     });
 
 
