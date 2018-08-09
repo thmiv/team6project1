8213b95f150a7365192759530b96aa46fcbf3520
Thomas <thmiv4@gmail.com>
2018-08-09 11:45:46 -0500

    made array to stor ingredients

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,20 @@
-// javascript (Food API?)
+// javascript (Spoontacular Food API)
+
+var userPantry = [];    // array to hold food supplies in user's pantry
+var userIngredients = [];    // array to hold ingredients inputed by user
+
+function getPantry(addPantryItem) {
+    userPantry.push(addPantryItem);
+    console.log(userPantry);
+}
+
+$( document ).ready(function() {
+
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+        var newItem = $("#pantry-input").val().trim();
+        getPantry(newItem);
+    });
+
+
+});
