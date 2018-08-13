a34b6ae145796ab5d4bf56448f8e410e748f5652
Sean Angle <seanangle@me.com>
2018-08-13 16:36:52 -0500

    Made a maps js file so that it would look more organized

---
+++ assets/javascript/maps.js
@@ -0,0 +1,53 @@
+//Map Section
+
+ 
+var map;
+var infowindow;
+
+var queryUrl = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCVf591AZ-evHODFReCvcQ56eAJZecmLgc";
+
+$.ajax({
+    url: queryUrl,
+    method: "POST",
+
+}).then(function(response){
+    initMap(response.location);
+
+});
+
+function initMap(userLocation) {
+
+  map = new google.maps.Map(document.getElementById('mapDiv'), {
+    center: userLocation,
+    zoom: 15
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: userLocation,
+    radius: 500,
+    type: ['store']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
